8c6e39df7ce87559d0130a0a75a62552f25377ec
Alumno <ulsa@iMac-de-Alumno.local>
2019-05-08 19:13:20 -0700

    Proyecto App Nutriologo

---
+++ ViewController/InicioSesionController.swift
@@ -0,0 +1,17 @@
+//
+//  InicioSesionController.swift
+//  NutriologoApp
+//
+//  Created by Alumno on 07/05/19.
+//  Copyright © 2019 Ulsa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InicioSesionController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var txtCorreo: UITextField!
+    @IBOutlet weak var txtContra: UITextField!
+    
+}
